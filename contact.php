b9a32f40d1c8a49f5950f31e987b691b93bc08e5
Simarjot Singh <sisingh@mcfadyen.com>
2024-04-19 23:29:12 -0400

    Latest changes on 19 apr

--- contact.php
+++ contact.php
@@ -1,82 +1,7 @@
-<!-- <!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<title>Crope</title>
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta name="description" content="Crope - Multipurpose agency website" />
-	<meta name="author" content="George_fx">
-	<meta name="keywords" content="" />
-	<link rel="icon" href="images/favicon.png">
-	<link rel="stylesheet" type="text/css" href="css/animate.css">
-	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
-	<link rel="stylesheet" type="text/css" href="css/style.css">
-	<link rel="stylesheet" type="text/css" href="css/color.css">
-	<link rel="stylesheet" type="text/css" href="css/responsive.css">
-</head>
 
-
-<body>
-
-	<div class="preloader">
-		<div class="loader-content">
-			<div class="logo">
-				<img src="images/logo.png" alt="">
-			</div>
-			<h2>is loading...</h2>
-			<img src="images/loader.gif" alt="">
-		</div>
-	</div>
-	
-	<div class="left-sidebar">
-		<a href="#" title="" class="menu-btn">
-			<svg width="34" height="34" viewBox="0 0 34 34" fill="none" xmlns="http://www.w3.org/2000/svg">
-				<path d="M0.711397 17.6361C-0.0696518 18.4171 -0.0696518 19.6834 0.711397 20.4645C1.49245 21.2455 2.75878 21.2455 3.53982 20.4645L20.5104 3.49393C21.2914 2.71288 21.2914 1.44655 20.5104 0.665498C19.7293 -0.115551 18.463 -0.11555 17.682 0.665498L0.711397 17.6361Z" fill="white"/>
-				<path d="M2.12558 31.7782C1.34453 30.9971 1.34453 29.7308 2.12558 28.9498L19.0961 11.9792C19.8772 11.1981 21.1435 11.1981 21.9246 11.9792C22.7056 12.7602 22.7056 14.0266 21.9246 14.8076L4.95401 31.7782C4.17296 32.5592 2.90663 32.5592 2.12558 31.7782Z" fill="white"/>
-				<path d="M13.4393 33.1924C12.6583 32.4113 12.6583 31.145 13.4393 30.364L30.4099 13.3934C31.1909 12.6124 32.4572 12.6124 33.2383 13.3934C34.0193 14.1745 34.0193 15.4408 33.2383 16.2218L16.2677 33.1924C15.4867 33.9734 14.2204 33.9734 13.4393 33.1924Z" fill="white"/>
-			</svg>
-		</a>
-		<ul class="mbl-menu">
-			<li><a href="#" title="">Linkedin</a></li>
-			<li><a href="#" title="">Behance</a></li>
-		</ul>
-	</div>
-
-	<div class="wrapper">
-
-		<header class="gradient-bg">
-			<div class="container">
-				<div class="header-content">
-					<div class="logo">
-						<a href="index.php" title="">
-							<img src="images/logo.png" alt="">
-						</a>
-					</div>
-					<nav>
-						<ul>
-							<li><a href="services.php" title="">Services</a></li>
-							<li><a href="cases.php" title="">Cases</a></li>
-							<li><a href="contact.php" title="">Contact us</a></li>
-						</ul>
-					</nav>
-					<div class="right-side-hd">
-						<span>+1 845 631 78 49</span>
-						<a href="#" title="" class="btn-default">Get a quote</a>
-					</div>
-					<a href="#" title="" class="menu-btn">
-						<svg width="34" height="34" viewBox="0 0 34 34" fill="none" xmlns="http://www.w3.org/2000/svg">
-							<path d="M0.711397 17.6361C-0.0696518 18.4171 -0.0696518 19.6834 0.711397 20.4645C1.49245 21.2455 2.75878 21.2455 3.53982 20.4645L20.5104 3.49393C21.2914 2.71288 21.2914 1.44655 20.5104 0.665498C19.7293 -0.115551 18.463 -0.11555 17.682 0.665498L0.711397 17.6361Z" fill="white"/>
-							<path d="M2.12558 31.7782C1.34453 30.9971 1.34453 29.7308 2.12558 28.9498L19.0961 11.9792C19.8772 11.1981 21.1435 11.1981 21.9246 11.9792C22.7056 12.7602 22.7056 14.0266 21.9246 14.8076L4.95401 31.7782C4.17296 32.5592 2.90663 32.5592 2.12558 31.7782Z" fill="white"/>
-							<path d="M13.4393 33.1924C12.6583 32.4113 12.6583 31.145 13.4393 30.364L30.4099 13.3934C31.1909 12.6124 32.4572 12.6124 33.2383 13.3934C34.0193 14.1745 34.0193 15.4408 33.2383 16.2218L16.2677 33.1924C15.4867 33.9734 14.2204 33.9734 13.4393 33.1924Z" fill="white"/>
-						</svg>
-					</a>
-					<div class="clearfix"></div>
-				</div>
-			</div>	
-		</header> -->
 <?php require 'common/header.php' ?>
 
-<section class="pager-sec text-right bg-darky pb-75">
+<section class="pager-sec text-right   pb-75">
 	<div class="container-fluid">
 		<ul>
 			<li><a href="index.php" title="">Home</a></li>
@@ -87,7 +12,7 @@
 
 <section class="sec-block2">
 	<h2 class="page-number bottom">01.</h2>
-	<div class="fixed-bg bg-darky"></div>
+	<div class="fixed-bg  "></div>
 	<div class="container">
 		<div class="contact-us-page">
 			<div class="row">
